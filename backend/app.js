84a30860a1be977c9f6d04295073c37cc056ac70
chalanika <chalanikasewwandi@gmail.com>
2019-10-04 11:41:50 +0530

    connected mongodb atlas to node

--- backend/app.js
+++ backend/app.js
@@ -75,5 +75,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, ()=>console.log("server start"));
+app.listen(3000, ()=>console.log("server started"));
+
 module.exports = app;
