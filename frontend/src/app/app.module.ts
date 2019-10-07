22d3528d51fa3f78ec1e207fa0cb6d26f54693e6
chalanika <chalanikasewwandi@gmail.com>
2019-10-07 20:16:15 +0530

    created rating form modal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LanguageTranslationModule } from './shared/modules/language-translation/language-translation.module';
 import {UserService} from './user.service';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
@@ -23,7 +23,9 @@ import {FileUploadModule} from 'ng2-file-upload';
         AppRoutingModule,
         FormsModule,
         FileUploadModule,
+        NgbModule,
         ReactiveFormsModule,
+        
     ],
     declarations: [AppComponent, FileComponent],
     providers: [AuthGuard , UserService],
---
+++ frontend/src/app/layout/dashboard/rating-form/rating-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-rating-form',
+  templateUrl: './rating-form.component.html',
+  styles: [`
+  .star {
+    font-size: 3rem;
+  }
+  .bad {
+    color: gray;
+  }
+  .filled.bad {
+    color: yellow;
+  }
+
+  .feedback{
+    border:0;
+    
+  }
+`]
+
+})
+export class RatingFormComponent implements OnInit {
+
+  closeResult: string;
+  currentRate = 0;
+  @ViewChild('content') content
+
+  constructor(private modalService: NgbModal) { }
+  ngOnInit() {
+    this.open();
+  }
+/*rating form modal*/
+  
+  open() {
+    this.modalService.open(this.content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+}
