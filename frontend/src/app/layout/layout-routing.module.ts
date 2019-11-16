cb3ef7266f4a9d2a5ea433b75b60ae52d575509d
chalanika <chalanikasewwandi@gmail.com>
2019-11-16 06:50:57 +0530

    error from shared

--- frontend/src/app/layout/layout-routing.module.ts
+++ frontend/src/app/layout/layout-routing.module.ts
@@ -11,13 +11,13 @@ const routes: Routes = [
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' },
             { path: 'post', loadChildren: './post/post.module#PostModule' },
-            { path: 'notifications', loadChildren: './notifications/notifications.module#NotificationsModule' },
-            { path: 'category', loadChildren: './category/category.module#CategoryModule' },
-            { path: 'report', loadChildren: './report/report.module#ReportModule' },
-            { path: 'appointment', loadChildren: './appointment/appointment.module#AppointmentModule' },
-            { path: 'calender', loadChildren: './calender/calender.module#CalenderModule' },
-            { path: 'clients', loadChildren: './clients/clients.module#ClientsModule' },
-            { path: 'lawyers', loadChildren: './lawyers/lawyers.module#LawyersModule' },
+            // { path: 'notifications', loadChildren: './notifications/notifications.module#NotificationsModule' },
+            // { path: 'category', loadChildren: './category/category.module#CategoryModule' },
+            // { path: 'report', loadChildren: './report/report.module#ReportModule' },
+            // { path: 'appointment', loadChildren: './appointment/appointment.module#AppointmentModule' },
+            // { path: 'calender', loadChildren: './calender/calender.module#CalenderModule' },
+            // { path: 'clients', loadChildren: './clients/clients.module#ClientsModule' },
+            // { path: 'lawyers', loadChildren: './lawyers/lawyers.module#LawyersModule' },
             { path: '**', redirectTo: 'not-found' }
         ]
     }
