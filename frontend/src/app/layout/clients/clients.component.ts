11b5aad997232257dac181e9f76bf90751744d78
chalanika <chalanikasewwandi@gmail.com>
2020-01-25 08:28:18 +0530

    get pending clients from back end to frontend

--- frontend/src/app/layout/clients/clients.component.ts
+++ frontend/src/app/layout/clients/clients.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { UserService } from 'src/app/user.service';
+import { AppointmentService } from 'src/app/core/services/appointment.service';
 
 @Component({
   selector: 'app-clients',
@@ -8,11 +10,34 @@ import { routerTransition } from '../../router.animations';
   animations: [routerTransition()]
 })
 export class ClientsComponent implements OnInit {
+  currentUser;
+  pendingClients;
 
-  constructor() { }
+  constructor(private _userService:UserService,private _appointmentService:AppointmentService) { }
 
   ngOnInit() {
-
+    this.getCurrentUser();
   }
 
+  getCurrentUser(){
+    this._userService.user().subscribe(
+      res=>{
+        this.currentUser = res; 
+        this.getPendingClients(this.currentUser._id);   
+      }, err => {
+        console.log(err);
+      }
+    );
+  }
+
+  getPendingClients(id){
+    this._appointmentService.getLawyersConfirmedAppointments(id).subscribe(
+      res=>{
+        this.pendingClients = res;
+        console.log(this.pendingClients);
+      },err=>{
+        console.log(err);
+      }
+    );
+  }
 }
